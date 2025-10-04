b03a579f3f9a7e7daa69cc1805eb3b576b3998ec
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-04 10:58:52 -0300

    Adiciona Algoritimo de Multiplicação
    Primeiro algoritimo proposto, multiplicar  2 números.

---
+++ Unidade01/Algoritimo_01_Multiplicação/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("*** MULTIPLICADOR DE NÚMEROS ***");
+        System.out.println("Digite dois números");
+        System.out.print("Primeiro número: ");
+        double n1 = sc.nextDouble();
+        System.out.print("Segundo número: ");
+        double n2 = sc.nextDouble();
+
+        double m = n1 * n2;
+
+        System.out.printf("Multiplicação = %.2f %n", m);
+
+        sc.close();
+    }
+    
+}
